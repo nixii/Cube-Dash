1f83e1c45b0f1a1fd520087f28ea51a2419fb787
nixii <161391079+nixii@users.noreply.github.com>
2026-07-15 10:19:46 -0700

    i've decided to pivot.

--- main.py
+++ main.py
@@ -10,21 +10,6 @@ async def main():
     global app
     while app.running:
         app.update()
-
-        mov = [0, 0]
-        if 'a' in app.keys_down:
-            mov[0] = -1
-        if 'd' in app.keys_down:
-            mov[0] = 1
-        if 'w' in app.keys_down:
-            mov[1] = -1
-        if 's' in app.keys_down:
-            mov[1] = 1
-    
-        dirs = util.move_and_collide(player, e, mov)
-
-        pygame.draw.rect(app.window, (255, 0, 255), player)
-        pygame.draw.rect(app.window, (255, 0, 255), e)
         app.render()
         await asyncio.sleep(0)
 
