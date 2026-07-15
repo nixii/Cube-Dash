72ba17fc2426b54dacb5e82f37387c31c9751c73
nixii <161391079+nixii@users.noreply.github.com>
2026-07-15 11:08:56 -0700

    spawning pipes.

--- main.py
+++ main.py
@@ -1,16 +1,122 @@
 
-import pygame, asyncio
-import app, util
+import pygame, asyncio, random
 
-app = app.App(800, 600, 1, "oo")
-player = pygame.Rect(0, 0, 40, 40)
-e = pygame.Rect(60, 60, 40, 40)
+# Constants
+GRAVITY = 3
+W_WIDTH = 800
+W_HEIGHT = 600
+PIPE_SPEED = 150
 
+# initialize pygame
+pygame.init()
+screen = pygame.display.set_mode((W_WIDTH, W_HEIGHT))
+pygame.display.set_caption("Cube Dash")
+ticks = pygame.time.get_ticks()
+
+# Is the app running?
+running = True
+
+# Player input
+key = False
+will_dash = False
+
+# Create the player entity
+player = pygame.Rect(0, 200, 40, 40)
+yvel = 0
+player_x = 0
+dash_time = 0
+
+# Game state
+gs = 1
+
+# "pipes"
+pipes = []
+pipe_timer = 0
+
+# Pipe class
+class Pipe():
+    def __init__(self, y):
+        self.x = W_WIDTH
+        self.y = y
+        self.r1 = pygame.Rect(self.x, 0, 80, y - 130)
+        self.r2 = pygame.Rect(self.x, y + 130, 80, W_HEIGHT - y - 130)
+    def render(self):
+        self.r1.x = self.x
+        self.r2.x = self.x
+        pygame.draw.rect(screen, (255, 0, 0), self.r1)
+        pygame.draw.rect(screen, (255, 0, 0), self.r2)
+
+# spawn a pipe
+def spawn_pipe():
+    global pipes
+    p = Pipe(random.randint(80, W_HEIGHT - 80))
+    pipes.append(p)
+    print(p.y)
+
+# Update the game
 async def main():
-    global app
-    while app.running:
-        app.update()
-        app.render()
+    global running, will_dash, key, yvel, ticks, gs, pipe_timer
+
+    # While it is running
+    while running:
+
+        ###########
+        ### DT calc
+        t = pygame.time.get_ticks()
+        dt = (t - ticks) / 1000.0
+        ticks = t
+
+        # spawn a pipe
+        pipe_timer -= dt
+        if pipe_timer <= 0:
+            spawn_pipe()
+            pipe_timer = 2
+
+        #################################
+        ### Update if the game is running
+        if gs == 1:
+
+            # input
+            key = False
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    running = False
+                elif e.type == pygame.KEYDOWN:
+                    key = True
+            
+            # gravity
+            yvel += GRAVITY
+
+            # key press
+            if key:
+                yvel = -500
+            
+            # update position
+            player.y += yvel * dt
+
+            # update pipes
+            for p in pipes:
+                p.x -= PIPE_SPEED * dt
+    
+        ################
+        ### Clear screen
+        screen.fill((0, 0, 0))
+
+        ##################
+        ### Render in-game
+        if gs == 1:
+
+            # Player
+            pygame.draw.rect(screen, (255, 255, 255), player)
+
+            # Render pipes
+            for p in pipes:
+                p.render()
+
+        ###########
+        ### Flip and await (since this will run in the browser)
+        pygame.display.flip()
         await asyncio.sleep(0)
 
+# Run!
 asyncio.run(main())
