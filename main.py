3838f1e4130c9c25546eb932cfb1692935c860a7
nixii <161391079+nixii@users.noreply.github.com>
2026-07-15 09:41:08 -0700

    Initial commit; simple window.

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import pygame, asyncio
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+
+async def main():
+    while running:
+
+        # event handling
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+        await asyncio.sleep(0)
+
+asyncio.run(main())
