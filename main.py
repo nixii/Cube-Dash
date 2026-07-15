7721ed589a539306488cd7a63f14ceeb2f42c936
nixii <161391079+nixii@users.noreply.github.com>
2026-07-15 10:05:57 -0700

    collision works!

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 
 import pygame, asyncio
+import app, util
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-
-running = True
+app = app.App(800, 600, 1, "oo")
+player = pygame.Rect(0, 0, 40, 40)
+e = pygame.Rect(60, 60, 40, 40)
 
 async def main():
-    global running
-    while running:
+    global app
+    while app.running:
+        app.update()
 
-        # event handling
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                running = False
+        mov = [0, 0]
+        if 'a' in app.keys_down:
+            mov[0] = -1
+        if 'd' in app.keys_down:
+            mov[0] = 1
+        if 'w' in app.keys_down:
+            mov[1] = -1
+        if 's' in app.keys_down:
+            mov[1] = 1
+    
+        dirs = util.move_and_collide(player, e, mov)
 
-        pygame.display.flip()
+        pygame.draw.rect(app.window, (255, 0, 255), player)
+        pygame.draw.rect(app.window, (255, 0, 255), e)
+        app.render()
         await asyncio.sleep(0)
 
 asyncio.run(main())
